4e74b5fdbc09bf30aa32964f9581ce2bd8f4b1b5
Animesh <animesh99dev@gmail.com>
2023-02-18 05:39:01 +0530

    fixed a bug

--- src/components/NavText.js
+++ src/components/NavText.js
@@ -1,6 +1,6 @@
 const NavText = (props) => {
   return (
-    <div className="nav-text sm:hidden md:block lg:block right-0 -bottom-20 font-extrabold">
+    <div className="nav-text sm:hidden md:block lg:block -right-20 -bottom-36 font-extrabold">
       {props.info}
     </div>
   );
